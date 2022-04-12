dde78c7f656187fdbab4bed918a486beab37c77b
rammfall <15bogdan17@gmail.com>
2022-04-12 03:22:39 +0300

    Complete migration

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const { join, resolve } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: resolve(__dirname, 'build'),
+    filename: '[name].[fullhash].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ["@babel/plugin-transform-runtime"]
+          }
+        }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new MiniCssExtractPlugin()
+  ],
+  devServer: {
+    static: {
+      directory: join(__dirname, 'src')
+    },
+    port: 3000,
+  }
+};
