59367d53848b5af79a9905c33c5ac5a5c5f77fc8
Anya Bu <annabukhtiiarova@gmail.com>
2022-06-11 10:37:08 +0300

    ejs for sections

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,15 @@
-const { join, resolve } = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const webpack = require("webpack");
+const { join, resolve } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
 
 module.exports = {
-  entry: ["fastify-webpack-hot/client", "./src/index.js"],
+  entry: ['fastify-webpack-hot/client', './src/index.js'],
   output: {
-    path: resolve(__dirname, "/build"),
-    filename: "[name].[fullhash].js",
+    path: resolve(__dirname, '/build'),
+    filename: '[name].[fullhash].js',
     clean: true,
-    publicPath: "/",
+    publicPath: '/',
   },
   module: {
     rules: [
@@ -17,10 +17,10 @@ module.exports = {
         test: /\.m?js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env"],
-            plugins: ["@babel/plugin-transform-runtime"],
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime'],
           },
         },
       },
@@ -28,40 +28,40 @@ module.exports = {
         test: /\.s[ac]ss$/i,
         use: [
           MiniCssExtractPlugin.loader,
-          "css-loader",
-          "postcss-loader",
-          "sass-loader",
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
         ],
       },
       {
         test: /\.(html)$/,
-        use: ["html-loader"],
+        use: ['html-loader'],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: './src/index.html',
     }),
-    // new HtmlWebpackPlugin({
-    //   template: "./src/promotions.html",
-    // }),
-    // new HtmlWebpackPlugin({
-    //   template: './src/cabinet.html'
-    // }),
-    // new HtmlWebpackPlugin({
-    //   template: "./src/pay.html",
-    // }),
-    // new HtmlWebpackPlugin({
-    //   template: "./src/basket.html",
-    // }),
+    new HtmlWebpackPlugin({
+      template: './src/promotions.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/cabinet.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/pay.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/basket.html',
+    }),
     new MiniCssExtractPlugin(),
   ],
   devServer: {
     static: {
-      directory: join(__dirname, "src"),
+      directory: join(__dirname, 'src'),
     },
     port: 3000,
   },
-  mode: "development",
+  mode: 'development',
 };
